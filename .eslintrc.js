c11b5b2a2dbbf59b338d840e2a500031b5da3fed
Matt Venables <matt@venabl.es>
2023-03-08 06:29:16 -0500

    Add eslint overrides for *.config.js files

---
+++ .eslintrc.js
@@ -0,0 +1,70 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "next/core-web-vitals",
+    "plugin:tailwindcss/recommended",
+    "prettier"
+  ],
+  overrides: [
+    /**
+     * Config files (e.g. jest.config.js, tailwind.config.js)
+     */
+    {
+      files: ["*.config.js"],
+      env: {
+        node: true
+      },
+      rules: {
+        "@typescript-eslint/no-var-requires": "off"
+      }
+    },
+    /**
+     * Jest Configuration
+     */
+    {
+      files: ["**/__tests__/**/*.{ts,tsx}", "**/*.test.{ts,tsx}"],
+      env: {
+        jest: true
+      },
+      extends: ["plugin:jest/recommended", "plugin:jest/style"],
+      plugins: ["jest"],
+      rules: {
+        "@typescript-eslint/no-non-null-assertion": "off"
+      }
+    }
+  ],
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        argsIgnorePattern: "^_",
+        caughtErrors: "none",
+        varsIgnorePattern: "^_"
+      }
+    ],
+    "import/order": [
+      "error",
+      {
+        "newlines-between": "always",
+        groups: [
+          ["builtin", "external"],
+          "internal",
+          ["sibling", "parent"],
+          "index",
+          "object",
+          "type"
+        ],
+        alphabetize: {
+          order: "asc"
+        }
+      }
+    ],
+    "sort-imports": [
+      "error",
+      {
+        ignoreDeclarationSort: true
+      }
+    ]
+  }
+}
